23ff940636354dec41a0b688b98453ca640e0e81
Sascha Kiefer <perl@intertivityNOSP4M.com>
2005-11-14 09:13:57 -0800

    import Crypt-SaltedHash 0.02 from CPAN
    git-cpan-module:   Crypt-SaltedHash
    git-cpan-version:  0.02
    git-cpan-authorid: ESSKAR
    git-cpan-file:     authors/id/E/ES/ESSKAR/Crypt-SaltedHash-0.02.tar.gz

--- lib/Crypt/SaltedHash.pm
+++ lib/Crypt/SaltedHash.pm
@@ -6,7 +6,7 @@ use Digest       ();
 
 use vars qw($VERSION);
 
-$VERSION = '0.01';
+$VERSION = '0.02';
 
 =head1 NAME
 
@@ -31,18 +31,151 @@ create salted (or seeded) hashed of clear text data. The original
 formalization of this concept comes from RFC-3112 and is extended by the use
 of different digital agorithms.
 
+=head1 ABSTRACT
+
+=head2 Setting the data
+
+The process starts with 2 elements of data: 
+
+=over
+
+=item * 
+
+a clear text string (this could represent a password for instance). 
+
+=item *
+
+the salt, a random seed of data. This is the value used to augment a hash in order to 
+ensure that 2 hashes of identical data yield different output.
+
+=back
+
+For the purposes of this abstract we will analyze the steps within code that perform the necessary actions 
+to achieve the end resulted hash. Cryptographers call this hash a digest. We will not however go into an explanation 
+of a one-way encryption scheme. Readers of this abstract are encouraged to get information on that subject by 
+their own.
+
+Theoretically, an implementation of a one-way function as an algorithm takes input, and provides output, that are both 
+in binary form; realistically though digests are typically encoded and stored in a database or in a flat text or XML file. 
+Take slappasswd5 for instance, it performs the exact functionality described above. We will use it as a black box compiled 
+piece of code for our analysis.
+
+In pseudocode we generate a salted hash as follows:
+
+    Get the source string and salt as separate binary objects
+    Concatenate the 2 binary values
+    Hash the concatenation into SaltedPasswordHash
+    Base64Encode(concat(SaltedPasswordHash, Salt))
+    
+We take a clear text string and hash this into a binary object representing the hashed value of the clear text string plus the random salt. 
+Then we have the Salt value, which are typically 4 bytes of purely random binary data represented as hexadecimal notation (Base16 as 8 bytes).
+
+Using SHA-1 as the hashing algorithm, SaltedPasswordHash is of length 20 (bytes) in raw binary form 
+(40 bytes if we look at it in hex). Salt is then 4 bytes in raw binary form. The SHA-1 algorithm generates 
+a 160 bit hash string. Consider that 8 bits = 1 byte. So 160 bits = 20 bytes, which is exactly what the 
+algorithm gives us. 
+
+The Base64 encoded final string representation of the binary result looks like: 
+
+    {SSHA}B0O0XSYdsk7g9K229ZEr73Lid7HBD9DX
+
+Take note here that the final output is a 32-byte string of data. The Base64 encoding process uses bit shifting, masking, and padding as per RFC-3548.
+
+A couple of examples of salted hashes using on the same exact clear-text string:
+
+    slappasswd -s testing123
+    {SSHA}72uhy5xc1AWOLwmNcXALHBSzp8xt4giL
+
+    slappasswd -s testing123
+    {SSHA}zmIAVaKMmTngrUi4UlS0dzYwVAbfBTl7
+
+    slappasswd -s testing123
+    {SSHA}Be3F12VVvBf9Sy6MSqpOgAdEj6JCZ+0f
+
+    slappasswd -s testing123
+    {SSHA}ncHs4XYmQKJqL+VuyNQzQjwRXfvu6noa
+
+4 runs of slappasswd against the same clear text string each yielded unique endresult hashes. 
+The random salt is generated silently and never made visible.
+
+=head2 Extracting the data
+
+One of the keys to note is that the salt is dealt with twice in the process. It is used once for the actual application of randomness to the 
+given clear text string, and then it is stored within the final output as purely Base64 encoded data. In order to perform an authentication 
+query for instance, we must break apart the concatenation that was created for storage of the data. We accomplish this by splitting 
+up the binary data we get after Base64 decoding the stored hash.
+
+In pseudocode we would perform the extraction and verification operations as such: 
+
+    Strip the hash identifier from the Digest
+    Base64Decode(Digest, 20)
+    Split Digest into 2 byte arrays, one for bytes 0 – 20(pwhash), one for bytes 21 – 32 (salt)
+    Get the target string and salt as separate binary object
+    Concatenate the 2 binary values
+    SHA hash the concatenation into targetPasswordHash
+    Compare targetPasswordHash with pwhash
+    Return corresponding Boolean value
+
+Our job is to split the original digest up into 2 distinct byte arrays, one of the left 20 (0 - 20 including the null terminator) bytes and 
+the other for the rest of the data. The left 0 – 20 bytes will represent the salted binary value we will us for a byte-by-byte data 
+match against the new clear text presented for verification. The string presented for verification will have to be salted as well. The rest 
+of the bytes (21 – 32) represent the random salt which when decoded will show the exact hex characters that make up the once randomly 
+generated seed.
+
+We are now ready to verify some data. Lets start with the 4 hashes presented earlier. We will run them through our code to extract the 
+random salt and then using that verify the clear text string hashed by slappasswd. First, lets do a verification test with an erroneous 
+password; this should fail the matching test: 
+
+    {SSHA}72uhy5xc1AWOLwmNcXALHBSzp8xt4giL Test123
+    Hash extracted (in hex): ef6ba1cb9c5cd4058e2f098d71700b1c14b3a7cc
+    Salt extracted (in hex): 6de2088b
+    Hash length is: 20 Salt length is: 4
+    Hash presented in hex: 256bc48def0ce04b0af90dfd2808c42588bf9542
+    Hashes DON'T match: Test123
+
+The match failure test was successful as expected. Now let’s use known valid data through the same exact code:
+
+    {SSHA}72uhy5xc1AWOLwmNcXALHBSzp8xt4giL testing123
+    Hash extracted (in hex): ef6ba1cb9c5cd4058e2f098d71700b1c14b3a7cc
+    Salt extracted (in hex): 6de2088b
+    Hash length is: 20 Salt length is: 4
+    Hash presented in hex: ef6ba1cb9c5cd4058e2f098d71700b1c14b3a7cc
+    Hashes match: testing123
+    
+The demystification of this functionality is evident. We see that salting hashed data does indeed add another layer of security to the 
+clear text one-way hashing process. But we also see that salted hashes should also be protected just as if the data was in clear text form. 
+Now that we have seen salted hashes actually work you should also realize that in code it is possible to extract salt values and use them 
+for various purposes. Obviously the usage can be on either side of the colored hat line, but the data is there.
+
 =head1 METHODS
 
 =over 4
 
-=item B<new([%options])>
+=item B<new( [%options] )>
 
 Returns a new Crypt::SaltedHash object.
 Possible keys for I<%options> are:
 
-- I<algorithm>: It's also possible to use common string representations of the
+=over
+
+=item *
+
+I<algorithm>: It's also possible to use common string representations of the
 algorithm (e.g. "sha256", "SHA-384"). If the argument is missing, SHA-1 will
 be used by default.
+
+=item *
+
+I<salt>: You can specify your on salt. You can either specify it as a sequence
+of charactres or as a hex encoded string of the form "HEX{...}". If the argument is missing,
+a random seed is provided for you (recommended).
+
+=item *
+
+I<salt_len>:  By default, the module assumes a salt length of 4 bytes (or 8, if it is encoded in hex). 
+If you choose a different length, you have to tell the I<validate> function how long your seed was.
+
+=back
 
 =cut
 
@@ -50,15 +183,14 @@ sub new {
     my ( $class, %options ) = @_;
 
     $options{algorithm} ||= 'SHA-1';
-    $options{salt}      ||= &generate_hex_salt();
+    $options{salt_len}  ||= 4;
+    $options{salt}      ||= &generate_hex_salt( $options{salt_len} * 2 );
 
     $options{algorithm} = uc( $options{algorithm} );
-    $options{algorithm} = 'SHA' if $options{algorithm} eq 'SHA-1';
+    $options{algorithm} .= '-1'
+      if $options{algorithm} =~ m!SHA$!;  # SHA => SHA-1, HMAC-SHA => HMAC-SHA-1
 
-    my $algorithm = $options{algorithm};
-    $algorithm = 'SHA-1' if $algorithm eq 'SHA';
-
-    my $digest = Digest->new($algorithm);
+    my $digest = Digest->new( $options{algorithm} );
     my $self   = {
         salt      => $options{salt},
         algorithm => $options{algorithm},
@@ -69,15 +201,7 @@ sub new {
     return bless $self, $class;
 }
 
-=item B<obj()>
-
-=cut
-
-sub obj {
-    shift->{digest};
-}
-
-=item B<add($data, ...)>
+=item B<add( $data, ... )>
 
 Logically joins the arguments into a single string, and uses it to
 update the current digest state. For more details see L<Digest>.
@@ -89,7 +213,37 @@ sub add {
     $self->obj->add(@_);
 }
 
+=item B<salt_bin()>
+
+Returns the salt in binary form.
+
+=cut
+
+sub salt_bin {
+    my ($self) = @_;
+
+    return $self->{salt} =~ m!^HEX{(.*)}$!i ? pack( "H*", $1 ) : $self->{salt};
+}
+
+=item B<salt_hex()>
+
+Returns the salt in hexadecimal form ('HEX{...}')
+
+=cut
+
+sub salt_hex {
+    my ($self) = @_;
+
+    return $self->{salt} =~ m!^HEX{(.*)}$!i
+      ? $self->{salt}
+      : 'HEX{' . join( '', unpack( 'H*', $self->{salt} ) ) . '}';
+}
+
 =item B<generate()>
+
+Generates the seeded hash. Uses the I<clone>-method of L<Digest> before actually performing
+the digest calculation, so adding more cleardata after a call of I<generate> to an instance of
+I<Crypt::SaltedHash> has the same effect as adding the data before the call of I<generate>.
 
 =cut
 
@@ -97,7 +251,7 @@ sub generate {
     my ($self) = @_;
 
     my $clone = $self->obj->clone;
-    my $salt  = pack( "H*", $self->{salt} );
+    my $salt  = $self->salt_bin;
 
     $clone->add($salt);
 
@@ -107,21 +261,46 @@ sub generate {
     return "{$scheme}$gen";
 }
 
-=item B<validate($hasheddata, $cleardata)>
+=item B<validate( $hasheddata, $cleardata, [$salt_len] )>
+
+Validates a hasheddata priviously generated against cleardata. I<$salt_len> defaults to 4 if not set.
+Returns 1 if the validation is successful, 0 otherwise.
 
 =cut
 
 sub validate {
-    my ( undef, $hasheddata, $cleardata ) = @_;
+    my ( undef, $hasheddata, $cleardata, $salt_len ) = @_;
+
+    # trim white-spaces
+    $hasheddata =~ s!^\s+!!;
+    $hasheddata =~ s!\s+$!!;
 
     my $scheme    = uc( &get_pass_scheme($hasheddata) );
     my $algorithm = &make_algorithm($scheme);
-    my $salt      = &extract_salt( &get_pass_hash($hasheddata) );
+    my $hash      = &get_pass_hash($hasheddata);
+    my $salt      = &extract_salt( $hash, $salt_len );
 
-    my $obj = __PACKAGE__->new( algorithm => $algorithm, salt => $salt );
+    my $obj = __PACKAGE__->new(
+        algorithm => $algorithm,
+        salt      => $salt,
+        salt_len  => $salt_len
+    );
     $obj->add($cleardata);
 
-    return $obj->generate eq $hasheddata;
+    my $gen_hasheddata = $obj->generate;
+    my $gen_hash       = &get_pass_hash($gen_hasheddata);
+
+    return $gen_hash eq $hash;
+}
+
+=item B<obj()>
+
+Returns a handle to L<Digest> object.
+
+=cut
+
+sub obj {
+    shift->{digest};
 }
 
 =back
@@ -135,7 +314,11 @@ I<none yet.>
 sub make_scheme {
 
     my $scheme = shift;
-    $scheme = "$1$2" if $scheme =~ m!(\w+)\-(\d+)!;
+
+    my @parts = split /-/, $scheme;
+    pop @parts if $parts[-1] eq '1';    # SHA-1 => SHA
+
+    $scheme = join '', @parts;
 
     return uc("S$scheme");
 }
@@ -143,9 +326,20 @@ sub make_scheme {
 sub make_algorithm {
 
     my $algorithm = shift;
-    if ( $algorithm =~ m!^{S(.*)}$! ) {
+           
+    if ( $algorithm =~ m!^S(.*)$! ) {
         $algorithm = $1;
-        $algorithm = "$1-$2" if $algorithm =~ m!(\w+)(\d+)!;
+        if ( $algorithm =~ m!(\w+)(\d+)! ) {
+
+            my $name   = uc($1);
+            my $digits = $2;
+
+            $name = "HMAC-$2" if $name =~ m!^HMAC(.*)$!;    # HMAC-SHA-1
+            $digits = "-$digits" unless $name =~ m!MD$!;    # MD2, MD4, MD5
+
+            $algorithm = "$name$digits";
+        }
+
     }
 
     return $algorithm;
@@ -157,7 +351,7 @@ sub get_pass_scheme {
 }
 
 sub get_pass_hash {
-    $_[0] =~ m/}([^\s]*)/;
+    $_[0] =~ m/}(.*)/;
     return $1;
 }
 
@@ -167,7 +361,7 @@ sub generate_hex_salt {
         "0", "1", "2", "3", "4", "5", "6", "7",
         "8", "9", "a", "b", "c", "d", "e", "f"
     );
-    my $length = 8;
+    my $length = shift || 8;
 
     my $salt = '';
     my $max  = scalar @keychars;
@@ -176,14 +370,17 @@ sub generate_hex_salt {
         $salt .= $keychars[ $skip + int( rand( $max - $skip ) ) ];
     }
 
-    return $salt;
+    return "HEX{$salt}";
 }
 
 sub extract_salt {
-    my $binhash = &MIME::Base64::decode_base64( $_[0] );
-    my $binsalt = substr( $binhash, length($binhash) - 4 );
 
-    return join( '', unpack( 'H*', $binsalt ) );
+    my ( $hash, $salt_len ) = @_;
+
+    my $binhash = &MIME::Base64::decode_base64($hash);
+    my $binsalt = substr( $binhash, length($binhash) - ( $salt_len || 4 ) );
+
+    return $binsalt;
 }
 
 =head1 SEE ALSO
--- t/Crypt-SaltedHash.t
+++ t/Crypt-SaltedHash.t
@@ -5,11 +5,55 @@
 
 # change 'tests => 1' to 'tests => last_test_to_print';
 
-use Test::More tests => 1;
-BEGIN { use_ok('Crypt::SaltedHash') };
+use strict;
+use Test::More tests => 11;
+BEGIN { use_ok('Crypt::SaltedHash') }
 
 #########################
 
-# Insert your test code below, the Test::More module is use()ed here so read
-# its man page ( perldoc Test::More ) for help writing this test script.
+my ( $csh, $salted, $valid );
 
+my %known_salts = (
+    'MD5'   => '{SMD5}vfwtsKpZn1kZ5WXDKCFqUTEyMzQ=',
+    'SHA-1' => '{SSHA}kRnWqCDFvZFoV7A6cTGBdq1Xv7cxMjM0',
+);
+
+foreach my $alg (qw/MD5 SHA-1/) {
+
+    $csh = Crypt::SaltedHash->new( algorithm => $alg );
+    $csh->add('secret');
+
+    $salted = $csh->generate;
+    $valid  = Crypt::SaltedHash->validate( $salted, 'secret' );
+
+    ok( $valid, "$alg: default test" );
+
+    $csh = Crypt::SaltedHash->new( algorithm => $alg, salt_len => 32 );
+    $csh->add('secret');
+
+    $salted = $csh->generate;
+    $valid  = Crypt::SaltedHash->validate( $salted, 'secret', 32 );
+
+    ok( $valid, "$alg: salt_len test" );
+
+    $csh = Crypt::SaltedHash->new( algorithm => $alg );
+
+    $csh->add('secret');
+    $salted = $csh->generate;
+    $csh->add('secret');
+    $salted = $csh->generate;
+
+    $valid = Crypt::SaltedHash->validate( $salted, 'secretsecret' );
+
+    ok( $valid, "$alg: generate test" );
+
+    $csh = Crypt::SaltedHash->new( algorithm => $alg, salt => '1234' );
+    $csh->add('secret');
+
+    ok( $csh->generate eq $known_salts{$alg}, "$alg: own bin-salt test" );
+
+    $csh = Crypt::SaltedHash->new( algorithm => $alg, salt => 'HEX{31323334}' );
+    $csh->add('secret');
+
+    ok( $csh->generate eq $known_salts{$alg}, "$alg: own hex-salt test" );
+}
